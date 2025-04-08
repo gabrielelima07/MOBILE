ea169d7028174346c2d1effdc83c7fa4ed1803ca
Gabriele Lima <gabriele.dibia07@gmail.com>
2025-04-07 21:33:50 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet, Platform } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function HomeScreen() {
+  return (
+  <View>
+
+  </View>
+  );
+}
+
+
